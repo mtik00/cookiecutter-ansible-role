8f404c1895ce6fb357cb3101253493cdfd8d8df2
Tim McFadden <mtik00@users.noreply.github.com>
2022-08-04 09:04:01 -0600

    (feat) adding direnv

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,16 +4,30 @@
 import os
 import shutil
 import sys
+from pathlib import Path
+
 
 REMOVE_DIRS = [
     '{% if cookiecutter.enable_github_actions != "yes" %}.github/workflows{% endif %}',
 ]
 
+# Adding .envrc directly to the template makes template development a pain since
+# direnv is enabled in the directoy above.  Therefore, create the file dynamically.
+DIRENV_ENABLED = '{% if cookiecutter.enable_direnv == "yes" %}true{% endif %}'
+DIRENV = """layout python python3
+
+[[ -f .secrets/env.sh ]] && source .secrets/env.sh
+
+PATH_add bin
+"""
 
 def main():
     for path in REMOVE_DIRS:
         if path and os.path.exists(path):
             shutil.rmtree(path)
+
+    if DIRENV_ENABLED == "true":
+        Path(".envrc").write_text(DIRENV)
 
 
 if __name__ == "__main__":
